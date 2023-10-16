4556dff290980e4806bc522eb493fd6c7ea66693
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2023-10-16 16:15:58 +0300

    change prod variables

--- config/config.js
+++ config/config.js
@@ -21,18 +21,18 @@ require('dotenv').config(); module.exports = {
     logging: false
   },
   production: {
-    username: process.env.DB_USER_PROD,
-    password: process.env.DB_PASS_PROD,
-    database: process.env.DB_PROD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
     dialect: 'postgres',
-    // dialectOptions: {
-    //   ssl: {
-    //     require: true,
-    //     rejectUnauthorized: false
-    //   },
-    //   logging: false
-    // }
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      },
+      logging: false
+    }
   }
 };
